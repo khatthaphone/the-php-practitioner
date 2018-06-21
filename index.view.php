673ee32fd2d351ae2fd5286b427a07767bba4c34
Khatthaphone <khatthaphonez@gmail.com>
2018-06-21 14:14:20 +0700

    add boolean condition

--- index.view.php
+++ index.view.php
@@ -18,11 +18,23 @@
   <h1>Task For The Day</h1>
 
   <ul>
-    <?php foreach ($task as $heading => $value): ?>
-      <li>
-        <strong><?= ucwords($heading); ?></strong>: <?= $value ?>
-      </li>
-    <?php endforeach; ?>
+    
+    <li>
+      <strong>Name: </strong> <?= $task['title']; ?>
+    </li>
+    
+    <li>
+      <strong>Due Date: </strong> <?= $task['due']; ?>
+    </li>
+    
+    <li>
+      <strong>Personal Responsible: </strong> <?= $task['assigned_to']; ?>
+    </li>
+    
+    <li>
+      <strong>Status: </strong> <?= $task['completed'] ? 'Complete' : 'Incomplete'; ?>
+    </li>
+
   </ul>
 
 </body>
