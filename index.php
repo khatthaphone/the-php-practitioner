55d2ecefce63465dfee8ba21ca3ef0aa523e48cb
Khatthaphone <khatthaphonez@gmail.com>
2018-06-22 17:17:42 +0700

    clean code

--- index.php
+++ index.php
@@ -5,18 +5,10 @@
  *  PDO
 */
 require 'Task.php';
+require 'functions.php';
 
-try {
-  $pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
-} catch(PDOException $e) {
-  die($e->getMessage());
-}
-
-$statement = $pdo->prepare('select * from todos');
-
-$statement->execute();
-
-$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+$pdo = connectToDb();
+$tasks = fetchAllTasks($pdo);
 
 // var_dump($tasks[0]->description);
 
