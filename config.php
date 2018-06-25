9ddde258aaeba618cabdad3a1be74a07311ed3d9
Khatthaphone <khatthaphonez@gmail.com>
2018-06-25 11:46:45 +0700

    add config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
   'database' => [
     'name' => 'mytodo',
     'username' => 'root',
-    'password' => 'root',
-    'connection' => 'mysql:host=127.0.0.1:8889',
+    'password' => '',
+    'connection' => 'mysql:host=127.0.0.1',
     'options' => [
       PDO::ATTR_ERRMODE => PDO_ERRMODE_EXCEPTION
     ]
