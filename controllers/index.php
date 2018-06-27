7cb3983c08a523fc780659988f4c22d6b9982d41
Khatthaphone <khatthaphonez@gmail.com>
2018-06-27 15:09:43 +0700

    array filter, map, column

--- controllers/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$tasks = $app['database']->selectAll('todos', 'Task');
-
-require 'views/index.view.php';
--- index.php
+++ index.php
@@ -1,20 +1,49 @@
 <?php
 
-/**
- *  Step 17:
- *  Dry Up Your Views
-*/
-
-$database = require 'core/bootstrap.php';
-
-require Router::load('routes.php')
-  ->direct(Request::uri());
-
-// $router = new Router;
-
-// require 'routes.php';
+class Post {
+  public $title;
+  public $published;
 
 
-// require $router->direct($uri);
+  public function __construct($title, $published) {
+    $this->title = $title;
+    $this->published = $published;
+  }
 
+}
 
+$posts = [
+  new Post('My First Post', true),
+  new Post('My Second Post', true),
+  new Post('My Third Post', true),
+  new Post('My Fourth Post', false)
+];
+
+$publishedPosts = array_filter($posts, function($post) {
+  return $post->published;
+});
+
+$unpublishedPosts = array_filter($posts, function($post) {
+  return !$post->published;
+});
+
+// $modified = array_map(function($post) {
+//   $post->published = true;
+//   return $post;
+// }, $posts);
+
+// foreach ($posts as $post) {
+//   $post->published = true;
+// }
+
+// $modified = array_map(function($post) {
+//   return (array) $post;
+// }, $posts);
+
+$titles = array_map(function($post) {
+  return ['title' => $post->title];
+}, $posts);
+
+// $titles = array_column($posts, 'title');
+
+var_dump($titles);
