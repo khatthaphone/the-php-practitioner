ea6adbaa753cbe9caefcdbe2214c42366dae7ff9
Khatthaphone <khatthaphonez@gmail.com>
2018-12-25 07:01:51 +0700

    step3 variables

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo 'Hello World';
+
+$name = "Bob";
+
+echo 'Hello, $name';
+// output: Hello, $name
+
+echo "Hello, $name";
+// output: Hello, Bob
+
+// run on terminal:
+// php -S localhost:8888
+
+?>
