4b9edc2da6399d7245ee50825cce4d5ab8f6711a
Khatthaphone <khatthaphonez@gmail.com>
2018-12-25 07:19:16 +0700

    step5 separate files  Changes to be committed: 	modified:   index.php 	copied:     index.php -> index.view.php

--- index.php
+++ index.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <style>
-  
-    header {
-      background: #e3e3e3;
-      padding: 2em;
-      text-align: center;
-    }
+<?php
 
-  </style>
-</head>
-<body>
-  <header>
-    <!-- <h1>
-      <?php
-        // echo 'Hello World';
+$greeting = "Hello, " . htmlspecialchars($_GET['name']);
 
-        // URL = localhost:port/?name=Bob
-
-        //$name = $_GET['name'];
-
-        //echo "Hello, $name";
-      ?>
-    </h1> -->
-
-    <!-- <h1>
-      <?php /*echo "Hello, " . $_GET['name']*/ ?>
-    </h1> -->
-
-    <h1>
-      <?= "Hello, " . htmlspecialchars($_GET['name']); ?>
-    </h1>
-
-  </header>
-</body>
-</html>
+require 'index.view.php';
+?>
